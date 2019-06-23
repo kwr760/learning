d1559db09738107c2610f5bb723ea394bd2e4ffe
kwr760 <kroy760@gmail.com>
2019-06-23 13:07:15 -0700

    -

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,43 +1,19 @@
 const path = require('path');
-const webpack = require('webpack');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  // mode: 'development',
   entry: './src/index.js',
-  // entry: {
-  //   index: './src/index.js'
-    // print: './src/print.js'
-  // },
-  // devtool: 'inline-source-map',
-  // devServer: {
-  //   contentBase: './dist'
-  // },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Caching'
-    }),
-    new webpack.HashedModuleIdsPlugin()
-  ],
   output: {
-    filename: '[name].[contenthash].js',
-    // chunkFilename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    // publicPath: '/'
+    filename: 'webpack-numbers.js',
+    library: 'webpackNumbers',
+    libraryTarget: 'umd'
   },
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
     }
   }
 };
