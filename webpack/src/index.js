c37c23283dec7da962f626b8e979f9d421198ad4
kwr760 <kroy760@gmail.com>
2019-06-23 11:19:38 -0700

    Caching

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,18 +1,14 @@
 import _ from 'lodash';
-import printMe from './print.js';
+// import Print from './print.js';
 
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  // element.onclick = Print.bind(null, 'Hello webpack!');
 
   return element;
 }
 
 document.body.appendChild(component());
+
